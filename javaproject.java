5eab515870355bd53af3fff8d8e846083c122442
Sharathkumar-Babu <89689080+Sharathkumar-Babu@users.noreply.github.com>
2021-09-04 18:17:35 +0530

    testing

---
+++ javaproject.java
@@ -0,0 +1,24 @@
+package javaproject;
+
+public class javaproject {
+
+	public static void main(String[] args) {
+		int a=3;
+		int b=4;
+		
+		int temp;
+		
+		temp=a;
+		a=b;
+				
+		b=temp;		
+			
+		System.out.println(a);
+		System.out.println(b);
+		
+		
+				
+
+	}
+
+}
