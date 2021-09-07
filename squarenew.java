2ea7a01d03692147f0039f14c2e07bc6c997628a
Sharathkumar-Babu <89689080+Sharathkumar-Babu@users.noreply.github.com>
2021-09-07 20:01:51 +0530

    checking

---
+++ squarenew.java
@@ -0,0 +1,18 @@
+package javaproject;
+
+
+
+	public class squarenew{  
+		public static void main(String args[]){  
+		int i=200;  
+		long l1=i;  
+		System.out.println(l1);  
+		
+
+		long l = 123456789L;
+		float f = l;
+		System.out.println(f);  
+
+	}
+
+}
